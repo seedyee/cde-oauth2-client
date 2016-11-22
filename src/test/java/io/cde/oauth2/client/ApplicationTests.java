ecebcf3bd25ee7d2694aa9ce3362afb03334dbf0
Fangcai Liao <liaofangcai@Fangcais-MacBook-Air.local>
2016-11-21 19:49:24 -0800

    新建项目测试用例入口文件

---
+++ src/test/java/io/cde/oauth2/client/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cde.oauth2.client;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Created by liaofangcai on 11/21/16.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class ApplicationTests {
+}
