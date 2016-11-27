ab5941872ce77d02d0b8266e760bac3802944718
Fangcai Liao <565107470@qq.com>
2016-11-28 00:17:17 +0800

    调整api，修改返回参数的设计和异常的处理

--- src/main/java/io/cde/oauth2/api/client/service/UserService.java
+++ src/main/java/io/cde/oauth2/api/client/service/UserService.java
@@ -2,8 +2,11 @@ package io.cde.oauth2.api.client.service;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
@@ -13,8 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import io.cde.oauth2.api.client.lang.ApiException;
-
 /**
  * Created by liaofangcai on 11/21/16.
  * 根据token请求用户数据.
@@ -22,35 +23,37 @@ import io.cde.oauth2.api.client.lang.ApiException;
 @Service
 public class UserService {
 
-    private final ParameterizedTypeReference<List<Object>> typeReferenc = new ParameterizedTypeReference<List<Object>>() { };
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
-    private final HttpHeaders headers = new HttpHeaders();
-
-    private URI url = null;
+    private static final ParameterizedTypeReference<List<Object>> typeReferenc = new ParameterizedTypeReference<List<Object>>() { };
 
     private final RestTemplate restTemplate = new RestTemplate();
+
     /**
      * 向服务器请求资源的url.
      */
-    @Value("${oauth2.api.client.requestUserInfoUrl}")
+    @Value("${io.cde.oauth2.api.client.requestUserUrl}")
     private String requestUserInfoUrl;
+
     /**
+     *
      * 根据token获取服务器资源信息(用户相关信息).
      * @param accessToken 服务器获取的token令牌密匙.
      * @return 用户相关信息数据.
+     * @throws URISyntaxException this URISyntaxException
      */
-    public List getUserInforByAccessToken(final String accessToken) {
-        try {
-            url = new URI(requestUserInfoUrl);
-            headers.set("Authorization", "token " + accessToken);
-            final RequestEntity<String> entity1 = new RequestEntity<>(headers, HttpMethod.GET, url);
-            final ResponseEntity<List<Object>> responseEntity = restTemplate.exchange(entity1, typeReferenc);
-            if (responseEntity.getStatusCode().is4xxClientError() || responseEntity.getStatusCode().is5xxServerError()) {
-                throw new ApiException(responseEntity);
-            }
-            return responseEntity.getBody();
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
+    public List<Object> getUserByAccessToken(final String accessToken) throws URISyntaxException {
+        List<Object> list = new ArrayList<Object>();
+        final HttpHeaders header = new HttpHeaders();
+        header.set("Authorization", "token " + accessToken);
+        final URI uri = new URI(this.requestUserInfoUrl);
+        final RequestEntity<String> entity = new RequestEntity<>(header, HttpMethod.GET, uri);
+        final ResponseEntity<List<Object>> responseEntity = this.restTemplate.exchange(entity, typeReferenc);
+        if (responseEntity.getStatusCode().is4xxClientError() || responseEntity.getStatusCode().is5xxServerError()) {
+            logger.error("API response entity is wrong about code");
+            return list;
         }
+        list = responseEntity.getBody();
+        return list;
     }
 }
