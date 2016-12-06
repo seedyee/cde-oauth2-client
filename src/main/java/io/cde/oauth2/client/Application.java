e32e79e7f9b5ccb976caca65f5ba51359623fd08
Fangcai Liao <565107470@qq.com>
2016-12-06 17:23:05 +0800

    修改类文件格式，去除引用中无用的包

--- src/main/java/io/cde/oauth2/client/Application.java
+++ src/main/java/io/cde/oauth2/client/Application.java
@@ -2,14 +2,12 @@ package io.cde.oauth2.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * Created by liaofangcai on 11/21/16.
  * 程序入口
  */
 @SpringBootApplication
-@ServletComponentScan
 public class Application {
 
     /**
