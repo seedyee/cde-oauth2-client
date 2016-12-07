b74c41d7f9beaa71015360b940ba59513977b8f4
Fangcai Liao <565107470@qq.com>
2016-12-07 19:21:07 +0800

    修改RequestAgent.java文件，添加对github OAuth服务端的请求逻辑处理

--- src/main/java/io/cde/oauth2/client/agent/RequestAgent.java
+++ src/main/java/io/cde/oauth2/client/agent/RequestAgent.java
@@ -1,14 +1,27 @@
 package io.cde.oauth2.client.agent;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import io.cde.oauth2.client.domain.AccessTokenRequestInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import io.cde.oauth2.client.builder.RequestInfoBuilder;
+import io.cde.oauth2.client.domain.AccessTokenRequestInfo;
+import io.cde.oauth2.client.domain.UserRequestInfo;
 
 /**
  * Created by liaofangcai on 11/21/16.
@@ -17,28 +30,68 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class RequestAgent {
 
+    private static final Logger logger = LoggerFactory.getLogger(RequestAgent.class);
+
+    private static final ParameterizedTypeReference<List<Object>> requestUserInfoTypeReference = new ParameterizedTypeReference<List<Object>>() { };
+
+    private static final ParameterizedTypeReference<Map<String, Object>> requestTokenTypeReference = new ParameterizedTypeReference<Map<String, Object>>() { };
+
     /**
      * 访问rest服务的客户端.
      */
     private RestTemplate restTemplate = new RestTemplate();
 
     /**
-     * 根据accessTokenRequest参数entity，跟服务器交换获取token.
-     * @param requestEntity 请求授权返回的code参数.
-     * @param typeReference 组装返回参数
-     * @return return responseEntity
+     * 获取请求参数对象的build.
      */
-    public ResponseEntity<Map<String, Object>> getAccessToken(final RequestEntity<AccessTokenRequestInfo> requestEntity, final ParameterizedTypeReference<Map<String, Object>> typeReference) {
-        return this.restTemplate.exchange(requestEntity, typeReference);
+    @Autowired
+    private RequestInfoBuilder requestInfoBuilder;
+
+    /**
+     * 请求token的服务器url.
+     */
+    @Value("${cde.oauth2.client.requestAccessTokenUrl}")
+    private String requestAccessTokenUrl;
+
+    /**
+     * 根据accessTokenRequest参数entity，跟服务器交换获取token.
+     * @param code 请求授权返回的code参数.
+     * @return return token
+     * @throws RestClientException this RestClientException
+     * @throws URISyntaxException this URISyntaxException
+     */
+    public String getAccessToken(final String code) throws RestClientException, URISyntaxException {
+        final AccessTokenRequestInfo accessTokenRequestInfo = this.requestInfoBuilder.buildAccessTokenRequestInfo(code);
+        final RequestEntity<AccessTokenRequestInfo> requestEntity = new RequestEntity<>(accessTokenRequestInfo, HttpMethod.POST, new URI(this.requestAccessTokenUrl));
+        final ResponseEntity<Map<String, Object>> responseEntity = this.restTemplate.exchange(requestEntity, requestTokenTypeReference);
+        final Map<String, Object> responseBody = responseEntity.getBody();
+        if (responseEntity.getStatusCode().is4xxClientError() || responseEntity.getStatusCode().is5xxServerError() || responseBody.containsKey("error")) {
+            logger.error("API response entity is wrong about code");
+            return null;
+        }
+        final String token = responseBody.get("access_token").toString();
+        return token;
     }
 
     /**
+     *
      * 根据entity获取服务器资源信息(用户相关信息).
-     * @param requestEntity 组装的请求数据和url
-     * @param typeReference 组装返回参数
-     * @return return responseEntity
+     * @param token this token
+     * @return return list
+     * @throws URISyntaxException this URISyntaxException
      */
-    public ResponseEntity<List<Object>> getUserInfo(final RequestEntity<String> requestEntity, final ParameterizedTypeReference<List<Object>> typeReference) {
-        return this.restTemplate.exchange(requestEntity, typeReference);
+    public List<Object> getUserInfo(final String token) throws URISyntaxException {
+        List<Object> list = new ArrayList<Object>();
+        final HttpHeaders header = new HttpHeaders();
+        header.set("Authorization", "token " + token);
+        final UserRequestInfo userRequestInfo = this.requestInfoBuilder.buildUserRequestInfo(token);
+        final RequestEntity<String> entity = new RequestEntity<>(header, HttpMethod.GET, new URI(userRequestInfo.getRequestUserUrl()));
+        final ResponseEntity<List<Object>> responseEntity = this.restTemplate.exchange(entity, requestUserInfoTypeReference);
+        if (responseEntity.getStatusCode().is4xxClientError() || responseEntity.getStatusCode().is5xxServerError()) {
+            logger.error("API response entity is wrong about code");
+            return list;
+        }
+        list = responseEntity.getBody();
+        return list;
     }
 }
